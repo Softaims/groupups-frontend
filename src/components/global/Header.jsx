c350b93e7b8a1abd7e7b52926fd1d736a3ce3ac6
zohaib-softaims <zohaib@thesoftaims.coms>
2025-05-09 13:24:20 +0500

    make header sticky

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <header className="w-full py-5 border-b border-[#024544]/30">
+    <header className="bg-[#030d13] sticky top-0 z-50 w-full py-5  border-b border-[#024544]/30">
       <div onClick={() => navigate("/")} className="ml-8 text-white cursor-pointer font-bold text-3xl">
         group<span className="text-[#4aa6a4]">ups</span>
       </div>
