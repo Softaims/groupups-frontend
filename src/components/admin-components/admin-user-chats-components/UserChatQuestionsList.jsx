8bb7a5c7ef2aa1ca38bfdbabe56905874a5ac0f1
Aliyan Jam <aliyan@thesoftaims.com>
2025-07-17 15:28:03 +0500

    Implement changes 4 and 5 for user chat interface

--- src/components/admin-components/admin-user-chats-components/UserChatQuestionsList.jsx
+++ src/components/admin-components/admin-user-chats-components/UserChatQuestionsList.jsx
@@ -3,7 +3,7 @@ import UserChatQuestionCard from "./UserChatQuestionCard";
 const UserChatQuestionsList = ({ responses }) => {
   return (
     <div className=" bg-[#0f1216] border border-[#1a1e24] rounded-lg shadow p-2 ">
-      <div className="max-h-[35git checkout -b change-requests-4-5-user-chat-uivh] overflow-y-auto space-y-4 p-4 ">
+      <div className="max-h-[35vh] overflow-y-auto space-y-4 p-4 ">
         {responses.map((question) => (
           <UserChatQuestionCard
             key={question.question_id}
